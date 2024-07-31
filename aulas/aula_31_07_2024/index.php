c7ef92308034b13e76b029d838e195f28c63c79c
Gelvazio <gelvazio@hotmail.com>
2024-07-31 19:11:19 -0300

    ok

--- aulas/aula_31_07_2024/index.php
+++ aulas/aula_31_07_2024/index.php
@@ -1,4 +1,7 @@
 <?php
-echo "<a href='atividades.php'>Atividades - 31-07-2024</a>";
+echo "<a href='atividades.php'>Atividades - 31-07-2024</a><br>";
+echo "Grupos:<br>";
+echo "grupo 01 - Vinicius e Ryan";
+echo "grupo 02 - Bruna, Romulo e Helton";
 
 
