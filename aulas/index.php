374254ebf16d29d38904480316fff7a1889c69da
Gelvazio <gelvazio@hotmail.com>
2024-08-05 21:36:28 -0300

    ok

--- aulas/index.php
+++ aulas/index.php
@@ -5,7 +5,7 @@ echo "<br>grupo 01 - Vinicius e Ryan";
 echo "<br>grupo 02 - Bruna, Romulo e Helton";
 
 echo "<hr>";
-echo "<a href='atividade_nocoes_basicas_php_02.php'>Atividades - 06-08-2024</a><br>";
+echo "<a href=''>Atividades - Fim do Slide 'SLIDE 04 - NOCOES BASICAS PHP 02' - 06-08-2024</a><br>";
 echo "Grupos:<br>";
 
 
