b2c55a340ad7ac1f7d0272eb3fb3f50a78a2b96e
Gelvazio <gelvazio@hotmail.com>
2024-08-07 21:40:04 -0300

    ok

--- aulas/aula_07_08_2024/index.php
+++ aulas/aula_07_08_2024/index.php
@@ -5,3 +5,7 @@ $usuario_login = "admin";
 $usuario_password = "1234";
 
 require_once("sessao.php");
+
+echo "<a href='exemploFormulario.php'>Exemplo Formulário</a>";
+echo '<hr>';
+echo "<a href='formulario.php'>Exemplo Formulário 02</a>";
